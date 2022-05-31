dfc25da2d3bfeb7d8794172521d2dc4e7d475336
Tukiko300 <105240267+Tukiko300@users.noreply.github.com>
2022-05-31 17:41:23 -0400

    Update info-instalarbot.js

--- plugins/info-instalarbot.js
+++ plugins/info-instalarbot.js
@@ -1,11 +1,6 @@
 import fs from 'fs'
 let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
-let text = `
-*—◉ 𝚃𝚄𝚃𝙾𝚁𝙸𝙰𝙻 𝙱𝙾𝚇𝙼𝙸𝙽𝙴 𝙷𝙾𝚂𝚃*
-> Tutorial: https://youtu.be/eC9TfKICpcY
-> Pagina Oficial: https://boxmineworld.com
-> Dashboard: https://dash.boxmineworld.com/home
-> Panel: https://panel.boxmineworld.com
+let text =
 
 ------------------------------------
 
@@ -19,8 +14,8 @@ let text = `
 > pkg install ffmpeg -y
 > pkg install imagemagick -y
 > pkg install yarn
-> git clone https://github.com/BrunoSobrino/TheMystic-Bot-MD
-> cd TheMystic-Bot-MD
+> git clone https://github.com/Tukiko300/V.35.45
+> cd V.35.45
 > npm install
 > yarn install 
 > npm install
@@ -32,7 +27,7 @@ contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: nul
 title: '𝙸𝙽𝙵𝙾𝚁𝙼𝙰𝙲𝙸𝙾𝙽 - 𝙸𝙽𝚂𝚃𝙰𝙻𝙰𝚁𝙱𝙾𝚃',
 body: 'ᴛʜᴇ ᴍʏsᴛᴄ ﹣ ʙᴏᴛ',         
 previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
-sourceUrl: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`}}})   
+sourceUrl: `https://github.com/BrunoSobrino/V.35.45`}}})   
 }
 handler.command = /^(instalarbot)/i
 export default handler
